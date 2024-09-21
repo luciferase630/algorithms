314315ee047d5cffb63bffc578bf012265c37b11
Lucifer <632077567@qq.com>
2024-09-21 15:30:56 +0800

    try

---
+++ algor/thirdrecursive.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+typedef long long ll;
+using namespace std;
+
+const int dx[8] = { +1  , +1, +2,  +2,  -1,  -1,  -2,  -2 };
+const int dy[8] = { +2,  -2,  +1,  -1,  +2,  -2,  +1,  -1 };//标记不能走的位置,一共八种组合
+bool blocked[21][21] = { false };
+int main(){
+	return 0;
+
+}
